6911f95dbf5fe4c47f68d7336cc82f0668f40218
shigeru0410 <pp3sr4@gmail.com>
2020-11-02 03:06:13 +0900

    ClassからFunctionへ

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,55 +3,49 @@ import Article from "./Article";
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
+  // this.state = {
+  //   isPublished: false,
+  //   count: 0
+  // }
 
-  componentDidMount() {
-    document.getElementById( 'counter').addEventListener('click', this.countUp)
-  }
+  // componentDidMount() {
+  //   document.getElementById( 'counter').addEventListener('click', this.countUp)
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10){
-      this.setState( {count: 0 })
-    }
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10){
+  //     this.setState( {count: 0 })
+  //   }
+  // }
 
-  componentDidCatch() {
-    document.getElementById( 'counter').removeEventListener('click', this.countUp)
-  }
+  // componentDidCatch() {
+  //   document.getElementById( 'counter').removeEventListener('click', this.countUp)
+  // }
 
-  togglePublished = () => {
-    this.setState( {
-      isPublished: !this.state.isPublished
-    })
-  };
+  // togglePublished = () => {
+  //   this.setState( {
+  //     isPublished: !this.state.isPublished
+  //   })
+  // };
 
-  countUp = () => {
-    this.setState( { count: this.state.count + 1})
-  }
+  // countUp = () => {
+  //   this.setState( { count: this.state.count + 1})
+  // }
 
-  render() {
-    const authorName = "おいおい"
-    return (
-      <>
-        <Article 
-          title = {"止まるんじゃねぇぞ"} 
-          isPublished = {this.state.isPublished} 
-          toggle = {() => this.togglePublished()}
-          count = {this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    )
-  }
+  return (
+    <>
+      <Article 
+        title = {"止まるんじゃねぇぞ"} 
+        // isPublished = {this.state.isPublished} 
+        // toggle = {() => this.togglePublished()}
+        // count = {this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  )
 }
 
 export default Blog
