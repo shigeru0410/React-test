12d94d12ff9389d6fd44ef5e011660f41edb81aa
shigeru0410 <pp3sr4@gmail.com>
2020-11-03 05:10:33 +0900

    useState, useEffectを使う

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const LikeButton = (props) => {
+  const [count, counter] = useState(0);
+
+  const countUp = () => {
+    counter(count + 1)
+  }
+
+  useEffect(() => {
+    document.getElementById( 'counter').addEventListener('click', countUp)
+  })
+
   return(
-    <button id = {"counter"}>いいね数: {props.count}</button>
+    <button id = {"counter"}>いいね数: {count}</button>
   )
 }
 
